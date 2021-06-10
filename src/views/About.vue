f87fdc5289221370d48161f12feef2e139d1f311
ysl <123@qq.com>
2021-06-10 17:37:56 +0800

    按钮样式

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,42 +1,125 @@
 <template>
-  <div class="about">
-    <input type="text" v-model="msg"><br/>
-    <ul>
-      <li v-for="(user, index) in users" :key="index">{{ user.name}}</li>
-    </ul>
+  <div class="statisContent" v-loading="loading">
+    <div class="chartNum">
+      <h3 class="orderTitle">订单总量</h3>
+      <div class="box-item">
+        <li
+          :class="{ 'number-item': !isNaN(item), 'mark-item': isNaN(item) }"
+          v-for="(item, index) in orderNum"
+          :key="index"
+        >
+          <span v-if="!isNaN(item)">
+            <i ref="numberItem">0123456789</i>
+          </span>
+          <span class="comma" v-else>{{ item }}</span>
+        </li>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'about',
-  data(){
+  data() {
     return {
-      msg: '',
-      users: [
-        {
-          name: '快车',
-          tag: '1'
-        },
-        {
-          name: '大车',
-          tag: '2'
-        },
-        {
-          name: '小车',
-          tag: '3'
-        },
-        {
-          name: '快小车',
-          tag: '4'
-        }
-      ]
-    }
+      loading: false,
+      orderNum: ["0", "0", "0", "0", "0", "0", "0", "0"],
+      count: 0,
+      tag: 456,
+    };
   },
-  filters: {
-    formatId: function(value){
-      console.log(value)
+  methods: {
+    // 设置文字滚动
+    setNumberTransform() {
+      const numberItems = this.$refs.numberItem; // 拿到数字的ref，计算元素数量
+      const numberArr = this.orderNum.filter((item) => !isNaN(item));
+      // 结合CSS 对数字字符进行滚动,显示订单数量
+      for (let index = 0; index < numberItems.length; index++) {
+        const elem = numberItems[index];
+        elem.style.transform = `translate(-50%, -${numberArr[index] * 10}%)`;
+      }
+    },
+    // 处理总订单数字
+    toOrderNum(num) {
+      num = num.toString();
+      // 把订单数变成字符串
+      if (num.length < 8) {
+        num = "0" + num; // 如未满八位数，添加"0"补位
+        this.toOrderNum(num); // 递归添加"0"补位
+      } else if (num.length === 8) {
+        // 订单数中加入逗号
+        //num = num.slice(0, 2) + "," + num.slice(2, 5) + "," + num.slice(5, 8);
+        this.orderNum = num.split(""); // 将其便变成数据，渲染至滚动数组
+      } else {
+        // 订单总量数字超过八位显示异常
+        this.$message.warning("订单总量数字过大，显示异常，请联系客服");
+      }
+    },
+    increaseNumber(add) {
+      let self = this;
+      self.count = self.count + add;
+      this.toOrderNum(self.count);
+      this.$nextTick(() => {
+        self.setNumberTransform(self.count);
+      });
+    },
+    getRandomNumber(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+  },
+  mounted() {
+    this.timer = setInterval(() => {
+      this.increaseNumber(23);
+    }, 3000);
+  },
+};
+</script>
+
+<style lang='scss'>
+.statisContent {
+  padding-top: 20px;
+  background-color: #000;
+  .number-item {
+    width: 41px;
+    height: 75px;
+    background: #ccc;
+    list-style: none;
+    margin-right: 5px;
+    background: rgba(250, 250, 250, 1);
+    border-radius: 4px;
+    border: 1px solid rgba(221, 221, 221, 1);
+    display: inline-block;
+    font-size: 60px;
+    & > span {
+      position: relative;
+      display: inline-block;
+      margin-right: 10px;
+      width: 100%;
+      height: 100%;
+      writing-mode: vertical-rl;
+      text-orientation: upright;
+      overflow: hidden;
+      & > i {
+        font-style: normal;
+        position: absolute;
+        top: 7px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        transition: transform 1s ease-in-out;
+        letter-spacing: 10px;
+      }
     }
   }
+  .number-item:last-child {
+    margin-right: 0;
+  }
+  .comma {
+    display: inline-block;
+    color: #fff;
+    font-size: 30px;
+  }
+  .mark-item {
+    display: inline-block;
+  }
 }
-</script>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="home">
-    <div class="a" @click="a">
-      <div class="b" @click.stop="b"></div>
-    </div>
-    <a href="https://www.baidu.com" @click.prevent="c" target="_blank">测试</a>
+    <div class="bor bor-hover">click me</div>
+    <div class="bor gradient">click me</div>
+    <div class="bor outline">click me</div>
   </div>
 </template>
 
@@ -16,17 +15,7 @@ export default {
   data() {
     return {};
   },
-  methods: {
-    a() {
-      alert("a");
-    },
-    b() {
-      alert("b");
-    },
-    c() {
-      alert("c");
-    }
-  }
+  methods: {},
 };
 </script>
 
@@ -41,6 +30,95 @@ export default {
     width: 50px;
     height: 50px;
     background: blue;
+  }
+}
+
+:root {
+  --borderColor: #03a9f3;
+}
+
+.bor {
+  width: 240px;
+  padding: 24px 0;
+  box-sizing: border-box;
+  text-align: center;
+}
+
+.bor + .bor {
+  margin-top: 30px;
+}
+
+.bor-hover {
+  position: relative;
+  border: 1px solid #03a9f3;
+
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    transition: 0.3s ease-in-out;
+  }
+
+  &::before {
+    top: -5px;
+    left: -5px;
+    border-top: 1px solid var(--borderColor);
+    border-left: 1px solid var(--borderColor);
+  }
+
+  &::after {
+    right: -5px;
+    bottom: -5px;
+    border-bottom: 1px solid var(--borderColor);
+    border-right: 1px solid var(--borderColor);
+  }
+
+  &:hover::before,
+  &:hover::after {
+    width: calc(100% + 9px);
+    height: calc(100% + 9px);
+  }
+}
+
+.gradient {
+  background: linear-gradient(90deg, #333 50%, transparent 0) repeat-x,
+    linear-gradient(90deg, #333 50%, transparent 0) repeat-x,
+    linear-gradient(0deg, #333 50%, transparent 0) repeat-y,
+    linear-gradient(0deg, #333 50%, transparent 0) repeat-y;
+  background-size: 4px 1px, 4px 1px, 1px 4px, 1px 4px;
+  background-position: 0 0, 0 100%, 0 0, 100% 0;
+  cursor: pointer;
+
+  &:hover {
+    animation: linearGradientMove 0.3s infinite linear;
+  }
+}
+
+.outline {
+  // border: 1px solid #333;
+  outline: 1px solid #333;
+  outline-offset: -1px;
+  transition: all 0.3s linear;
+  cursor: pointer;
+
+  &:hover {
+    // border: 1px solid transparent;
+    outline: 1px solid transparent;
+    background: linear-gradient(90deg, #333 50%, transparent 0) repeat-x,
+      linear-gradient(90deg, #333 50%, transparent 0) repeat-x,
+      linear-gradient(0deg, #333 50%, transparent 0) repeat-y,
+      linear-gradient(0deg, #333 50%, transparent 0) repeat-y;
+    background-size: 4px 1px, 4px 1px, 1px 4px, 1px 4px;
+    background-position: 0 0, 0 100%, 0 0, 100% 0;
+    animation: linearGradientMove 0.3s infinite linear;
+  }
+}
+
+@keyframes linearGradientMove {
+  100% {
+    background-position: 4px 0, -4px 100%, 0 -4px, 100% 4px;
   }
 }
 </style>

845cfeed03d19d976877cfcc234e4a10eeee03b4
ysl <123@qq.com>
2021-07-09 16:16:21 +0800

    分支提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="bor bor-hover">click me</div>
     <div class="bor gradient">click me</div>
     <div class="bor outline">click me</div>
-    <div>测试</div>
+    <div>测试合并</div>
   </div>
 </template>
 

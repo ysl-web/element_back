8eae6fd92df19b1cd942b8c6ddcea3f3242b84b3
ysl <123@qq.com>
2021-07-09 16:46:30 +0800

    重置1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="bor bor-hover">click me</div>
     <div class="bor gradient">click me</div>
     <div class="bor outline">click me</div>
-    <div>测试合并</div>
+    <div>重置测试1</div>
   </div>
 </template>
 
